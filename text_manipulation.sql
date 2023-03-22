aa821e2bb8b2f326d55f9d1fbd7faf22fb22439f
blazejblaszczak <71590378+blazejblaszczak@users.noreply.github.com>
2023-03-22 15:30:00 +0100

    Create text_manipulation.sql
    query added

---
+++ text_manipulation.sql
@@ -0,0 +1,3 @@
+-- splitting text based on chosen string part and position
+
+SPLIT_PART(description, 'ID ', 2) -- text is split based on string in second position, function returns part chosen in third position
